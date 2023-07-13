af09b723de3611e39af12bc0470a330193ef3f15
EmanuelaPau <uapame@gmail.com>
2023-07-13 15:13:20 +0200

    Add class Movie

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+<!-- Create un file index.php in cui:
 è definita una classe ‘Movie’ :
+
  => all'interno della classe sono dichiarate delle variabili d'istanza
  => all'interno della classe è definito un costruttore
  => all'interno della classe è definito almeno un metodo
+
 vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 Bonus 1:
 Modificare la classe Movie in modo che accetti piú di un genere.
@@ -11,3 +13,27 @@ Bonus 2:
 creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
 mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
 organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. -->
+
+<?php
+class Movie
+{
+    public $id;
+    public $name;
+    public $director;
+    public $vote;
+    public $sinopsis;
+
+    function __construct($movieName, $movieDirector, $movieVote, $movieSinopsis)
+    {
+        $this->name = $movieName;
+        $this->director = $movieDirector;
+        $this->vote = $movieVote;
+        $this->sinopsis = $movieSinopsis;
+    }
+    function createId()
+    {
+        $this->id = rand(0, 999);
+    }
+}
+
+?>
